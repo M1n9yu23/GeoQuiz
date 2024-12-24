dac822812e83b8862a9832afa9e99502e8e3624f
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-12-24 16:53:31 +0900

    [feat]: 화면 회전을 하면 실행 되는 레이아웃 리소스 추가.

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called" )
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -51,6 +55,31 @@ class MainActivity : AppCompatActivity() {
 
         updateQuestion() // 맨 처음 실행 했을 때 보여 주기 위해서 호출함.
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun checkAnswer(userAnswer: Boolean){
